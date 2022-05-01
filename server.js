faa468fde81441d611471c53c4c1b6afc3f97310
dimas <dprcontractor@gmail.com>
2022-05-01 16:46:16 -0400

    bugs

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
+  useNewUrlParser: false,
   useFindAndModify: false
 });
 
