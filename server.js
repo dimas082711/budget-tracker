f8aba957d779521f12141fcc09fd0ae36704f1b7
dimas <dprcontractor@gmail.com>
2022-05-01 16:47:27 -0400

    bugs

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: false,
-  useFindAndModify: false
-});
+// mongoose.connect(MONGODB_URI, {
+//   useNewUrlParser: false,
+//   useFindAndModify: false
+// });
 
 // routes
 app.use(require("./routes/api.js"));
